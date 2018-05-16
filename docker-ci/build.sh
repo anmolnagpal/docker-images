bf88c8d4b1b65ba71b5d9227ee509d6121835bad
Anmol Nagpal <ianmolnagpal@gmail.com>
2018-05-16 11:29:36 +0400

    update

--- docker-ci/build.sh
+++ docker-ci/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -t anmolnagpal/devops-ci:0.0.2 .
+docker build -t anmolnagpal/devops-ci:0.0.1 .
 
 if [[ $? != 0 ]]; then
     echo "DevOps CI Build failed."
     exit 1
 fi
 
-docker push anmolnagpal/devops-ci:0.0.2
+docker push anmolnagpal/devops-ci:0.0.1
