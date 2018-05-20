5638b204d13ac6beed3e3b4e84f8f33f2dbf891b
temitope.oteyowo <temitope.oteyowo@tajawal.com>
2018-05-20 15:14:09 +0400

    update

---
+++ devops-ci/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#docker build -t anmolnagpal/devops-ci:1.9.$CIRCLE_BUILD_NUM .
+#if [[ $? != 0 ]]; then
+#    echo "DevOps CI Build failed."
+#    exit 1
+#fi
+#
+#docker push anmolnagpal/devops-ci:1.9.$CIRCLE_BUILD_NUM
--- docker-ci/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t anmolnagpal/devops-ci:0.0.1 .
-if [[ $? != 0 ]]; then
-    echo "DevOps CI Build failed."
-    exit 1
-fi
-
-docker push anmolnagpal/devops-ci:0.0.1
