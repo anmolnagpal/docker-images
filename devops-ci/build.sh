22e1f67a122054c8316d2bcfe1e80089b5d6f684
temitope.oteyowo <temitope.oteyowo@tajawal.com>
2018-05-21 17:06:01 +0400

    update

--- devops-ci/build.sh
+++ devops-ci/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -t anmolnagpal/devops-ci:0.0.1 .
+docker build -t gentitope/devops-ci:0.0.1 .
 
 if [[ $? != 0 ]]; then
     echo "DevOps CI Build failed."
     exit 1
 fi
 
-docker push anmolnagpal/devops-ci:0.0.1
+docker push gentitope/devops-ci:0.0.1
--- devops-machine/build.sh
+++ devops-machine/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -t anmolnagpal/devops-machine:1.8.1 .
+docker build -t anmolnagpal/devops-machine:1.8.2 .
 
 if [[ $? != 0 ]]; then
     echo "DevOps Build failed."
     exit 1
 fi
 
-docker push anmolnagpal/devops-machine:1.8.1
+docker push anmolnagpal/devops-machine:1.8.2
