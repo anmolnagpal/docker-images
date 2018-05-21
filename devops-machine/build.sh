6b68c2b9430963d2e23486a5e57cf927b30acc18
temitope.oteyowo <temitope.oteyowo@tajawal.com>
2018-05-21 17:07:39 +0400

    update

--- devops-machine/build.sh
+++ devops-machine/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -t anmolnagpal/devops-machine:1.8.2 .
+docker build -t anmolnagpal/ops-machine:1.8.2 .
 
 if [[ $? != 0 ]]; then
     echo "DevOps Build failed."
     exit 1
 fi
 
-docker push anmolnagpal/devops-machine:1.8.2
+docker push anmolnagpal/ops-machine:1.8.2
