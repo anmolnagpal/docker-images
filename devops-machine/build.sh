42947073ff97c777efab7ffbfca54fa5bec6dec7
Anmol Nagpal <ianmolnagpal@gmail.com>
2019-08-11 21:14:50 +0200

    cleanup

--- devops-machine/build.sh
+++ devops-machine/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -t anmolnagpal/devops-machine:1.8.4 .
+docker build -t anmolnagpal/devops-machine:0.0.1 .
 
 if [[ $? != 0 ]]; then
     echo "DevOps Build failed."
     exit 1
 fi
 
-docker push anmolnagpal/devops-machine:1.8.4
+docker push anmolnagpal/devops-machine:0.0.1
